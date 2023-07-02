72f0219fc9d31ca36693cff2bfa771c90611982e
lvp0110 <110590002+lvp0110@users.noreply.github.com>
2023-07-02 23:43:32 +0300

    Add files via upload

---
+++ script/db.js
@@ -0,0 +1,95 @@
+let floorBtn = document.getElementById('floor');
+let ceilingBtn = document.getElementById('ceiling');
+let frameBtn= document.getElementById('frame');
+let partitionBtn = document.getElementById('partition');
+let formBtnConstr = document.querySelector('.form');
+let textChoosse = document.querySelector('.constr_choose');
+let textConstr = document.querySelector('.constr_text');
+let formListBtnConstr = document.querySelector('.form_constr_list_btn');
+let constrTypesLadelF = document.getElementById('F');
+let constrTypesLadelC = document.getElementById('C');
+let constrTypesLadelL = document.getElementById('L');
+let constrTypesLadelW = document.getElementById('W');
+
+let stopBtn = 0;
+let constrTypes = document.querySelector('.constr_types');
+let buttonCostrType = document.querySelector('button_cstr_type');
+
+function prinButton (data,select) {
+       let typeButton = data.map((el)=>`<button type="button" class="constr_list" value="${el.id}">${el.title}</button>`).join('');
+       select.innerHTML = typeButton;
+   }
+function filtrData (data,param){
+          return data.filter((el)=>el.id_type == param);
+      }
+function hideBtns(arrElements){
+   arrElements.forEach((element) => element.style.display = 'none'); 
+}
+
+floorBtn.onclick = function(){
+   insulationButton.disabled = stopBtn === 0;
+   textConstr.style.display = 'none';
+   textChoosse.style.display = 'block';
+   formBtnConstr.style.display = 'none';
+   floorBtn.style.display = 'none';
+   constrTypesLadelF.style.display = 'block';
+   ceilingBtn.style.display = 'none';
+   frameBtn.style.display = 'none';
+   partitionBtn.style.display = 'none';
+   createConstrBtns(this);
+}
+ceilingBtn.onclick = function(){
+   insulationButton.disabled = stopBtn === 0;
+   ceilingBtn.style.display = 'none';
+   formBtnConstr.style.display = 'none';
+   textConstr.style.display = 'none';
+   constrTypesLadelC.style.display = 'block';
+   textChoosse.style.display = 'block';
+   floorBtn.style.display = 'none';
+   frameBtn.style.display = 'none';
+   partitionBtn.style.display = 'none';
+   createConstrBtns(this);
+}
+frameBtn.onclick = function(){
+   insulationButton.disabled = stopBtn === 0;
+   frameBtn.style.display = 'none';
+   textConstr.style.display = 'none';
+   textChoosse.style.display = 'block';
+   floorBtn.style.display = 'none';
+   floorBtn.style.display = 'none';
+   textConstr.style.display = 'none';
+   formBtnConstr.style.display = 'none';
+   constrTypesLadelL.style.display = 'block';
+
+   ceilingBtn.style.display = 'none';
+   partitionBtn.style.display = 'none';
+   createConstrBtns(this);
+}
+partitionBtn.onclick = function(){
+   insulationButton.disabled = stopBtn === 0;
+   partitionBtn.style.display = 'none';
+   textConstr.style.display = 'none';
+   textChoosse.style.display = 'block';
+   formBtnConstr.style.display = 'none';
+   constrTypesLadelW.style.display = 'block';
+   floorBtn.style.display = 'none';
+   ceilingBtn.style.display = 'none';
+   frameBtn.style.display = 'none';
+   createConstrBtns(this);
+}
+
+function createConstrBtns(constrBtn){
+   switch(constrBtn.id){
+      case 'floor': prinButton(filtrData(Constructions,'F'),formListBtnConstr);break;
+      case 'ceiling': prinButton(filtrData(Constructions,'C'),formListBtnConstr);break;
+      case 'frame': prinButton(filtrData(Constructions,'L'),formListBtnConstr);break;
+      case 'partition': prinButton(filtrData(Constructions,'W'),formListBtnConstr);break;
+   }
+};
+
+
+
+
+
+
+
---
+++ script/main.js
@@ -0,0 +1,345 @@
+// const buttonStart = [
+//     {
+//         id: 600, 
+//         title: 'ПОЛ',
+//         description: 'Название раздела',
+//         id_type: 'F'
+//     }, 
+//     {
+//         id: 500, 
+//         title: 'ПОТОЛОК',
+//         description: 'Название раздела',
+//         id_type: 'C'
+//     }, 
+//     {
+//         id: 200, 
+//         title: 'ОБЛИЦОВКА',
+//         description: 'Название раздела',
+//         id_type: 'L'
+//     }, 
+//     {
+//         id: 100, 
+//         title: 'ПЕРЕГОРОДКИ',
+//         description: 'Название раздела',
+//         id_type: 'W'
+//     }, 
+// ]
+ 
+const Constructions = [
+    // {
+    //     id: 100, 
+    //     title: 'ПЕРЕГОРОДКИ',
+    //     description: 'Название раздела',
+    //     id_type: 'W'
+    // },
+    {
+        id: 101, 
+        title: 'Перегородка на каркасе 50 мм',
+        description: 'Перегородка на одинарном каркасе 50 мм',
+        id_type: 'W'
+    },
+    {
+        id: 102, 
+        title: 'Перегородка на каркасе 75 мм',
+        description: 'Перегородка на одинарном каркасе 75 мм',
+        id_type :'W'
+    },
+    {
+        id: 103, 
+        title: 'Перегородка на каркасе 100 мм',
+        description: 'Перегородка на одинарном каркасе 100 мм',
+        id_type:'W'
+    },
+    {
+        id: 104, 
+        title: 'Перегородка на каркасе Wave 100 мм',
+        description: 'Перегородка на одинарном каркасе Виброфлекс-Wave 100 мм',
+        id_type:'W'
+    },
+    {
+        id: 105, 
+        title: 'Перегородка на каркасе 2x50 мм',
+        description: 'Перегородка на двойном (независимом) сдвоенном каркасе 2x50 мм на раздельных основаниях звукоизолирующих полов',
+        id_type:'W'
+    },
+    {
+        id: 106, 
+        title: 'Перегородка на каркасе 2x75 мм',
+        description: 'Перегородка на двойном (независимом) сдвоенном каркасе 2x75 мм на раздельных основаниях звукоизолирующих полов',
+        id_type:'W'
+    },
+    {
+        id: 107, 
+        title: 'Перегородка на каркасе 2x100 мм',
+        description: 'Перегородка на двойном (независимом) сдвоенном каркасе 2x100 мм на раздельных основаниях звукоизолирующих полов',
+        id_type:'W'
+    },
+    {
+        id: 108, 
+        title: 'Перегородка системы Саундлайн dB-X AL',
+        description:'Перегородка системы Саундлайн dB-X AL на одинарном каркасе 50 мм',
+        id_type:'W'
+    },
+    // {
+    //     id: 200, 
+    //     title: 'ОБЛИЦОВКА',
+    //     description: 'Название раздела',
+    //     id_type:'L'
+    // },
+    {
+        id: 201, 
+        title: 'ЗИПС-Вектор',
+        description: 'Звукоизолирующая система ЗИПС-Вектор, смонтированная на стене',
+        id_type:'L'
+    },
+    {
+        id: 202, 
+        title: 'ЗИПС-Модуль',
+        description: 'Звукоизолирующая система ЗИПС-Модуль, смонтированная на стене',
+        id_type:'L'
+    },
+    {
+        id: 203, 
+        title: 'ЗИПС-III-Ультра',
+        description: 'Звукоизолирующая система ЗИПС-III-Ультра, смонтированная на стене',
+        id_type:'L'
+    },
+    {
+        id: 204, 
+        title: 'ЗИПС-Z4',
+        description: 'Звукоизолирующая система ЗИПС-Z4, смонтированная на стене',
+        id_type:'L',
+    },
+    {
+        id: 205, 
+        title: 'ЗИПС-Синема',
+        description: 'Звукоизолирующая система ЗИПС-Синема, смонтированная на стене',
+        id_type:'L'
+    },
+    {
+        id: 206, 
+        title: 'ЗИПС-Слим',
+        description: 'Звукоизолирующая система ЗИПС-Слим, смонтированная на стене',
+        id_type:'L'
+    },
+    // {
+    //     id: 500, 
+    //     title: 'ПОТОЛОК',
+    //     description: 'Название раздела',
+    //     id_type:'C'
+    // },
+    {
+        id: 201, 
+        title: 'ЗИПС-Вектор',
+        description: 'Звукоизолирующая система ЗИПС-Вектор, смонтированная на потолке',
+        id_type:'C'
+    },
+    {
+        id: 202, 
+        title: 'ЗИПС-Модуль',
+        description: 'Звукоизолирующая система ЗИПС-Модуль, смонтированная на потолке',
+        id_type:'C'
+    },
+    {
+        id: 203, 
+        title: 'ЗИПС-III-Ультра',
+        description: 'Звукоизолирующая система ЗИПС-III-Ультра, смонтированная на потолке',
+        id_type:'C'
+    },
+    {
+        id: 204, 
+        title: 'ЗИПС-Z4',
+        description: 'Звукоизолирующая система ЗИПС-Z4, смонтированная на потолке',
+        id_type:'C',
+    },
+    {
+        id:205, 
+        title: 'ЗИПС-Синема',
+        description: 'Звукоизолирующая система ЗИПС-Синема, смонтированная на потолке',
+        id_type:'C'
+    },
+    {
+        id: 401, 
+        title: 'Облицовка на каркасе 50 мм',
+        description: 'Облицовка на независимом сдвоенном каркасе 50 мм',
+        id_type:'L'
+    },
+    {
+        id: 402, 
+        title: 'Облицовка на каркасе 75 мм',
+        description: 'Облицовка на независимом каркасе 75 мм',
+        id_type:'L'
+    },
+    {
+        id: 403, 
+        title: 'Облицовка на каркасе 100 мм',
+        description: 'Облицовка на независимом каркасе 100 мм',
+        id_type:'L'
+    },
+    {
+        id: 404, 
+        title: 'Облицовка c применением Виброфлекс-Коннект ПС',
+        description: 'Облицовка на каркасе ПП 60/27 с применением креплений Виброфлекс-Коннект ПС',
+        id_type:'L'
+    },
+    {
+        id: 405, 
+        title: 'Облицовка с применением Виброфлекс-КС',
+        description: 'Облицовка на каркасе ПП 60/27 с применением креплений Виброфлекс-КС',
+        id_type:'L'
+    },
+  
+    {
+        id: 501, 
+        title: 'Потолок на креплениях Виброфлекс-Коннект ПП',
+        description: 'Подвесной потолок, смонтированный на креплениях Виброфлекс-Коннект ПП',
+        id_type:'C'
+    },
+    {
+        id: 502, 
+        title: 'Потолок на креплениях Виброфлекс-К15',
+        description: 'Подвесной потолок, смонтированный на креплениях Виброфлекс-К15',
+        id_type:'C'
+    },
+    {
+        id: 503, 
+        title: 'Потолок на креплениях Виброфлекс-К15 с удлинителями',
+        description: 'Подвесной потолок, смонтированный на креплениях Виброфлекс-К15 с удлинителями из профиля ПП 60/27',
+        id_type:'C'
+    },
+    // {
+    //     id: 600, 
+    //     title: 'ПОЛ',
+    //     description:'Название раздела',
+    //     id_type:'F'
+    // },
+    {
+        id: 601, 
+        title: 'Пол Акуфлекс-Супер, паркет, 15мм',
+        description:'Паркетная доска 15 мм на материале Акуфлекс-Супер',
+        id_type:'F'
+    },
+    {
+        id: 602, 
+        title: 'Пол Акуфлекс-Супер, ламинат, 8 мм',
+        description: 'Ламинат 8 мм на материале Акуфлекс-Супер',
+        id_type:'F'
+    },
+    {
+        id: 603, 
+        title: 'Пол Акуфлекс-Супер, стяжка, 65 мм',
+        description:'Звукоизолирующий пол на материале Акуфлекс-Супер',
+        id_type:'F'
+    },
+    {
+        id: 604, 
+        title: 'Пол Шуманет-100Комби',
+        description: 'Звукоизолирующий пол на материале Шуманет-100Комби',
+        id_type:'F'
+    },
+    {
+        id: 605, 
+        title: 'Пол Шуманет-100Гидро',
+        description:'Звукоизолирующий пол на материале Шуманет-100Гидро',
+        id_type:'F'
+    },
+    {
+        id: 606, 
+        title: 'Пол Шумопласт',
+        description: 'Звукоизолирующая выравнивающая смесь Шумопласт',
+        id_type:'F'
+    },
+    {
+        id: 607, 
+        title: 'Пол Шумостоп-С2/К2, один слой',
+        description: 'Звукоизолирующий пол с одним слоем системы плит Шумостоп-С2/К2',
+        id_type:'F'
+    },
+    {
+        id: 608, 
+        title: 'Пол Шумостоп-С2/К2, два слоя',
+        description: 'Звукоизолирующий пол с двумя слоями системы плит Шумостоп-С2/К2',
+        id_type:'F'
+    },
+    {
+        id: 609, 
+        title: 'Пол Шумостоп-К2, один слой',
+        description: 'Звукоизолирующий пол с одним слоем материала Шумостоп-К2',
+        id_type:'F'
+    },
+    {
+        id: 610, 
+        title: 'Пол Шумостоп-К2, два слоя',
+        description: 'Звукоизолирующий пол с двумя слоями материала Шумостоп-К2',
+        id_type:'F'
+    },
+    {
+        id: 611, 
+        title: 'Пол Шуманет-Термо',
+        description: 'Звукоизолирующий пол с одним слоем материала Шуманет-Термо',
+        id_type:'F'
+    },
+    {
+        id: 612, 
+        title: 'Пол Шумостоп-Техно',
+        description: 'Звукоизолирующий пол с применением панелей Шумостоп-Техно',
+        id_type:'F'
+    },
+    {
+        id: 613, 
+        title: 'ЗИПС-ПОЛ Вектор',
+        descriptio: 'Сборная звукоизолирующая система ЗИПС-ПОЛ Вектор',
+        id_type:'F'
+    },
+    {
+        id: 614, 
+        title: 'ЗИПС-ПОЛ Модуль',
+        description: 'Сборная звукоизолирующая система ЗИПС-ПОЛ Модуль',
+        id_type:'F'
+    },
+    {
+        id: 615, 
+        title: 'Звукоизолирующий пол на лагах',
+        description:'Звукоизолирующий пол на лагах',
+        id_type:'F'
+    },
+];
+
+let insulationButton = document.getElementById('insulation');
+let acousticButton = document. getElementById('acoustic');
+let headerText = document.querySelector('.header_text');
+let constrText = document.querySelector('.constr_text');
+let formConstr = document.querySelector('form');
+
+let flag = 1;
+insulationButton.onclick = function(){
+    if(flag == 1) goToConstrTypes();
+    else if( flag == 2) returnToMainMenu();
+    
+}
+function goToConstrTypes(){
+    acousticButton.style.display = 'none';
+    headerText.style.display = 'none';
+    formConstr.style.position = 'block';
+    formConstr.style.display = 'block';
+    constrText.style.display = 'block';
+    flag = 2;
+}
+function returnToMainMenu(){
+    acousticButton.style.display = 'block';
+    headerText.style.display = 'block';
+    formConstr.style.position = 'block';
+    formConstr.style.display = 'none';
+    constrText.style.display = 'none';
+    flag = 1;
+}  
+
+acousticButton.onclick = function(){
+    headerText.style.display = 'none';
+    acousticButton.style.top = 'auto';
+    formConstr.style.display = 'none';
+    constrText.style.display = 'block';
+    insulationButton.style.display = 'none';
+}
+/*Кнопка назад*/
+
